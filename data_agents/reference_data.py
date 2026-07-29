c266e0a76362e24a86162d9355d041df9bc30764
Ned Glass <ned.glass.03@gmail.com>
2026-07-29 09:47:28 +0100

    feat: add get_reference_data to read company reference JSON

---
+++ data_agents/reference_data.py
@@ -0,0 +1,9 @@
+import json
+import os
+
+REFERENCE_DATA_PATH = os.path.join(os.path.dirname(__file__), "..", "reference_data", "company_data.json")
+
+def get_reference_data(ticker: str) -> dict:
+    with open(REFERENCE_DATA_PATH) as f:
+        data = json.load(f)
+    return data.get(ticker, {})
